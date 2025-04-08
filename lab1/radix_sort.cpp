3c8c1fe20f646a08d9e426eff76f56bbffcfed15
luckyabatur <dekhterenkodmitrii@gmail.com>
2025-04-08 22:49:49 +0300

    concept 2

--- lab1/radix_sort.cpp
+++ lab1/radix_sort.cpp
@@ -2,26 +2,44 @@
 
 
 const int VALUE_LENGTH = 2049;
-const int BASE_SIZE = 70;
+const int BASE_SIZE = 70; //бред
 const int DIGITS_COUNT = 10;
 const int COUNTY_FIELD_SIZE = 3;
 const int CITY_FIELD_SIZE = 5;
 const int PHONE_FIELD_SIZE = 10;
+const int BITS_COUNT = 8;
+const unsigned int MASK = 255;
 
 enum class FieldType {COUNTRY, CITY, PHONE};
 
-void CountingSort(unsigned int* mainArray, int digitsCount = DIGITS_COUNT){
+
+struct PhoneNumber{
+    unsigned int country;
+    unsigned int city;
+    unsigned int phone;
+    char value[VALUE_LENGTH];
+};
+
+void CountingSort(unsigned int* array, PhoneNumber* baseArray,
+                  const int arraySize = BASE_SIZE,
+                  const int digitsCount = DIGITS_COUNT){
+    int dopArray[BASE_SIZE] {};
+
+}
 
 
 
 
-})
-
-
-void RadixSort(unsigned int* array, int arraySize = BASE_SIZE){
-
-
-
+void RadixSort(unsigned int* array, PhoneNumber* baseArray,
+               int arraySize = BASE_SIZE){
+    for (int i = 0; i < sizeof(int)/BITS_COUNT; i++){
+        unsigned int countingArray[BASE_SIZE];
+        for (int j = 0; j < BASE_SIZE; j++){
+            countingArray[j] = array[j] & MASK;
+            countingArray[j] << BITS_COUNT;
+        }
+        CountingSort(countingArray, baseArray);
+    }
 }
 
 void MainRadixSort(PhoneNumber* array, FieldType field){
@@ -34,12 +52,7 @@ void MainRadixSort(PhoneNumber* array, FieldType field){
 
 }
 
-struct PhoneNumber{
-    unsigned int country;
-    unsigned int city;
-    unsigned int phone;
-    char value[VALUE_LENGTH];
-};
+
 
 int main(){
 
